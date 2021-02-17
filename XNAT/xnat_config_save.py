5baa852ed261aded567dfc385d4a0a4a2e1c1a05
mark <18689435+markendr@users.noreply.github.com>
2021-02-17 05:41:06 +0000

    allow siteurl to set http or https, added script to save site config

--- XNAT/xnat_config_save.py
+++ XNAT/xnat_config_save.py
@@ -8,6 +8,7 @@ ap.add_argument("-s", "--siteurl", required=True, help="Site URL (http[s]://<hos
 ap.add_argument("-u", "--user", required=True, help="Username")
 ap.add_argument("-p", "--password", required=True, help="Password")
 ap.add_argument("-f", "--configfile", required=True, help="Configuration json file")
+ap.add_argument("--pp", action='store_true', help="Pretty-print to json file")
 
 args = vars(ap.parse_args())
 
@@ -15,20 +16,24 @@ siteurl = args["siteurl"]
 configfile = args["configfile"]
 user = args["user"]
 password = args["password"]
+pp = args["pp"]
 
 api_endpoint = siteurl.rstrip("/")+"/xapi/siteConfig"
 headers = {"Content-Type": "application/json", "Accept": "*/*"}
-try:
-    data = open(configfile, "r").read()
-except Exception as err:
-    print(f"Can't read file: {err}")
-    exit(1)
     
 try:
-    response = requests.post(api_endpoint, data=data, headers=headers, auth=requests.auth.HTTPBasicAuth(user,password))
+    response = requests.get(api_endpoint, headers=headers, auth=requests.auth.HTTPBasicAuth(user,password))
     response.raise_for_status()
 except Exception as err:    
     print(f"Error occurred: {err}")
+    exit(1)
+
+data = json.dumps(response.json(), indent=4) if pp else response.text
+
+try:
+    open(configfile, "w").write(data)
+except Exception as err:
+    print(f"Can't write file: {err}")
     exit(1)
     
 exit()

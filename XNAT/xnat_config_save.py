f1664779ab9c0ccb07bfefc9211327e59a3e8dac
mark <18689435+markendr@users.noreply.github.com>
2021-02-19 05:22:22 +0000

    added property option and stdout output if no json file provided

--- XNAT/xnat_config_save.py
+++ XNAT/xnat_config_save.py
@@ -7,7 +7,8 @@ ap = argparse.ArgumentParser()
 ap.add_argument("-s", "--siteurl", required=True, help="Site URL (http[s]://<host>[:<port>])")
 ap.add_argument("-u", "--user", required=True, help="Username")
 ap.add_argument("-p", "--password", required=True, help="Password")
-ap.add_argument("-f", "--configfile", required=True, help="Configuration json file")
+ap.add_argument("-f", "--configfile", default=None, help="Configuration json file")
+ap.add_argument("--property", default=None, help="Get value of selected property only")
 ap.add_argument("--pp", action='store_true', help="Pretty-print to json file")
 
 args = vars(ap.parse_args())
@@ -16,9 +17,10 @@ siteurl = args["siteurl"]
 configfile = args["configfile"]
 user = args["user"]
 password = args["password"]
+prop = args["property"]
 pp = args["pp"]
 
-api_endpoint = siteurl.rstrip("/")+"/xapi/siteConfig"
+api_endpoint = '/'.join(filter(None, [siteurl.rstrip("/"), "xapi/siteConfig", prop]))
 headers = {"Content-Type": "application/json", "Accept": "*/*"}
     
 try:
@@ -28,12 +30,18 @@ except Exception as err:
     print(f"Error occurred: {err}")
     exit(1)
 
-data = json.dumps(response.json(), indent=4) if pp else response.text
+if (prop is None) and (pp):
+    data = json.dumps(response.json(), indent=4)
+else:
+    data = response.text
 
-try:
-    open(configfile, "w").write(data)
-except Exception as err:
-    print(f"Can't write file: {err}")
-    exit(1)
+if configfile is None:
+    print(data)
+else:
+    try:
+        open(configfile, "w").write(data)
+    except Exception as err:
+        print(f"Can't write file: {err}")
+        exit(1)
     
 exit()
